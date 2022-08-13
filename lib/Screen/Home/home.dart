8bbf887f56eaf5f231b46134eb1c4168ab2d202d
SaminYeaser <saminyeaser1@gmail.com>
2022-08-14 00:36:37 +0600

    home is fixed for tablet screen greater than 1200

--- lib/Screen/Home/home.dart
+++ lib/Screen/Home/home.dart
@@ -190,7 +190,8 @@ class HomePage extends StatelessWidget {
           ),
         ),
       );
-    }else if(size.height > 700){
+    }
+    else if(size.height < 800){
       return  Scaffold(
         backgroundColor: primaryColorLight,
         body: SafeArea(
@@ -287,6 +288,149 @@ class HomePage extends StatelessWidget {
 
                       Positioned(
                         top: (size.height /1.74),
+                        left: (size.width /8),
+                        child: InkWell(
+                            onTap: (){
+                              Get.to(AttendenceReport());
+                            },
+                            child: circleRow(size, 'A', (size.width /12), (size.height /1.74))),
+                      ),
+
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.arrow_back,color: darkBlue,),
+                        Text('Prev',style: TextStyle(
+                            color: darkBlue
+                        ),)
+                      ],
+                    ),
+                    InkWell(
+                      onTap: (){
+                        Get.to( AttendenceReport());
+                      },
+                      child: Row(
+                        children: [
+                          Icon(Icons.arrow_forward,color: darkBlue,),
+                          Text('Next',style: TextStyle(
+                              color: darkBlue
+                          ),)
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+    }
+    else if(size.height >= 1200){
+      return  Scaffold(
+        backgroundColor: primaryColorLight,
+        body: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text('Rayhan Uddin', style: TextStyle(
+                            fontSize: 20, color: Colors.black,
+                            fontWeight: FontWeight.w600
+                        ),),
+                        SizedBox(height: 2,),
+                        Text('01502020222', style: TextStyle(
+                            fontSize: 16, color: Colors.grey
+                        ),)
+                      ],
+                    ),
+                    Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(150)
+                      ),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.white
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(Icons.edit_outlined,color: darkBlue,),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Center(
+                  child: Stack(
+                    children: [
+                      Container(
+                        height: size.height / 1.3,
+                        transform: Matrix4.translationValues(-(size.width /2.5), 0.0, 0.0),
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.white
+                        ),
+                      ),
+                      Container(
+                        transform: Matrix4.translationValues(-(size.width /3.7), (size.height /6.3), 0.0),
+                        child: const CircleAvatar(
+                          radius: 280,
+                          backgroundImage: AssetImage('images/person.jpg',),
+                        ),
+                      ),
+                      Positioned(
+                        top: (size.height /22),
+                        left: (size.width /8),
+                        child: InkWell(
+                            onTap: (){
+                              Get.to(AttendenceReport());
+                            },
+                            child: circleRow(size, 'E', (size.width /4), (size.height /6))),
+                      ),
+
+                      Positioned(
+                          top: (size.height/6),
+                          left: (size.width /2.2),
+                          child: circleRow(size, 'D', (size.width /2.7), (size.height /8))),
+                      Positioned(
+                        top: (size.height /2.6),
+                        left: (size.width /1.75),
+                        child: InkWell(
+                          onTap: (){
+                            Get.to(AttendenceReport());
+                          },
+                          child: circleRow(size, 'C', 240.0, 210.0),
+                        ),
+                      ),
+
+                      Positioned(
+                          top: (size.height /1.7),
+                          left: (size.width /2.3),
+                          child: InkWell(
+                              onTap: (){
+                                Get.to(AttendenceReport());
+                              },
+                              child: circleRow(size, 'B', (size.width /2.5), (size.height /2)))),
+
+                      Positioned(
+                        top: (size.height /1.45),
                         left: (size.width /8),
                         child: InkWell(
                             onTap: (){
