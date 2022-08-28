e1319413ccf25b3f3a259c2846e72861c3197550
MirFahim <mirfahim@aapbd.com>
2022-08-28 10:26:25 +0600

    from main

--- lib/Screen/Home/home.dart
+++ lib/Screen/Home/home.dart
@@ -11,6 +11,7 @@ import '../../utils.dart';
 import 'package:get/get.dart';
 
 import '../Attendence_report/attendence_report.dart';
+import '../leave/leave_front.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -124,7 +125,7 @@ class HomePage extends StatelessWidget {
                         left: (size.width / 8),
                         child: InkWell(
                             onTap: () {
-                              Get.to(AttendenceReport());
+                              Get.to(LeaveFront());
                             },
                             child: circleRow(size, 'Leave', 20.0, 50.0)),
                       ),
@@ -297,7 +298,7 @@ class HomePage extends StatelessWidget {
                         left: (size.width / 8),
                         child: InkWell(
                             onTap: () {
-                              Get.to(AttendenceReport());
+                              Get.to(LeaveFront());
                             },
                             child: circleRow(size, 'Leave', 20.0, 50.0)),
                       ),
@@ -384,6 +385,170 @@ class HomePage extends StatelessWidget {
           ),
         ),
       );
+    } else if (size.height < 850) {
+      return Scaffold(
+        backgroundColor: primaryColorLight,
+        body: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          'Rayhan Uddin85',
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(
+                          height: 2,
+                        ),
+                        Text(
+                          '01502020222',
+                          style: TextStyle(fontSize: 16, color: Colors.grey),
+                        )
+                      ],
+                    ),
+                    Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(150)),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.white),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.edit_outlined,
+                            color: darkBlue,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Center(
+                  child: Stack(
+                    children: [
+                      Container(
+                        height: size.height / 1.3,
+                        transform: Matrix4.translationValues(
+                            -(size.width / 2.5), 0.0, 0.0),
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.white),
+                      ),
+                      Container(
+                        transform: Matrix4.translationValues(
+                            -(size.width / 3.7), (size.height / 5.5), 0.0),
+                        child: const CircleAvatar(
+                          radius: 150,
+                          backgroundImage: AssetImage(
+                            'images/person.jpg',
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: (size.height / 9.5),
+                        left: (size.width / 8),
+                        child: InkWell(
+                            onTap: () {
+                              Get.to( LeaveFront());
+                            },
+                            child: circleRow(size, 'Leave', 20.0, 50.0)),
+                      ),
+                      Positioned(
+                          top: (size.height / 5.0),
+                          left: (size.width / 2.3),
+                          child: InkWell(
+                              onTap: () {
+                                Get.to(AttendenceReport());
+                              },
+                              child: circleRow(size, 'Attendance',
+                                  (size.width / 2.7), (size.height / 8)))),
+                      Positioned(
+                        top: (size.height / 2.8),
+                        left: (size.width / 1.8),
+                        child: InkWell(
+                          onTap: () {
+                            Get.to(Task());
+                          },
+                          child: circleRow(size, 'Tasks', 240.0, 210.0),
+                        ),
+                      ),
+                      Positioned(
+                          top: (size.height / 2.05),
+                          left: (size.width / 2.34),
+                          child: InkWell(
+                              onTap: () {
+                                Get.to(ExpenseList());
+                              },
+                              child: circleRow(size, 'Expenses',
+                                  (size.width / 2.5), (size.height / 2)))),
+                      Positioned(
+                        top: (size.height / 1.80),
+                        left: (size.width / 8),
+                        child: InkWell(
+                            onTap: () {
+                              Get.to(NotificationPage());
+                            },
+                            child: circleRow(size, 'Notice', (size.width / 12),
+                                (size.height / 1.74))),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.arrow_back,
+                          color: darkBlue,
+                        ),
+                        Text(
+                          'Prev',
+                          style: TextStyle(color: darkBlue),
+                        )
+                      ],
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Get.to(AttendenceReport());
+                      },
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.arrow_forward,
+                            color: darkBlue,
+                          ),
+                          Text(
+                            'Next',
+                            style: TextStyle(color: darkBlue),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+
+
+
     } else if (size.height < 900) {
       return Scaffold(
         backgroundColor: primaryColorLight,
@@ -399,7 +564,7 @@ class HomePage extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: const [
                         Text(
-                          'Rayhan Uddin',
+                          'Rayhan Uddin9',
                           style: TextStyle(
                               fontSize: 20,
                               color: Colors.black,
@@ -458,7 +623,7 @@ class HomePage extends StatelessWidget {
                         left: (size.width / 8),
                         child: InkWell(
                             onTap: () {
-                              Get.to(AttendenceReport());
+                              Get.to(LeaveFront());
                             },
                             child: circleRow(size, 'Leave', 20.0, 50.0)),
                       ),
@@ -545,6 +710,9 @@ class HomePage extends StatelessWidget {
           ),
         ),
       );
+
+
+
     } else if (size.height >= 1200) {
       return Scaffold(
         backgroundColor: primaryColorLight,
@@ -560,7 +728,7 @@ class HomePage extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: const [
                         Text(
-                          'Rayhan Uddin',
+                          'Rayhan Uddin12',
                           style: TextStyle(
                               fontSize: 20,
                               color: Colors.black,
@@ -619,7 +787,7 @@ class HomePage extends StatelessWidget {
                         left: (size.width / 8),
                         child: InkWell(
                             onTap: () {
-                              Get.to(Approved());
+                              Get.to(LeaveFront());
                             },
                             child: circleRow(size, 'Leave', (size.width / 4),
                                 (size.height / 6))),
