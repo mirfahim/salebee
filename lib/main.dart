87a01f4d9fd6c28c991181428863e0bdcb782708
Samin hishabee <samin@hishabee.io>
2022-09-17 00:53:21 +0600

    problem fixes in all over the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'package:salebee/Screen/SplashScreen.dart';
 import 'package:provider/provider.dart';
 
 import 'Provider/Login/login_provider.dart';
-import 'Screen/Authentication/login_page.dart';
-import 'Screen/Home/home.dart';
 import 'bottomNav.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp( const MyApp());
 }
 
 class MyApp extends StatelessWidget {
