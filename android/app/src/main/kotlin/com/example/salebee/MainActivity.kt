9efca2e10c88dcfb119aa4cb2b70f4d14dd18217
mirfahim <ridoyfahim92@gmail.com>
2022-10-27 19:20:42 +0600

    mir 27 - 12.50

--- android/app/src/main/kotlin/com/example/salebee/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/salebee/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.salebee
+package com.salebee.crm
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,45 @@ import 'package:get/get.dart';
 import 'package:salebee/Screen/SplashScreen.dart';
 import 'package:provider/provider.dart';
 import 'package:salebee/Service/sharedPref_service.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'Provider/Login/provider_manager.dart';
 import 'Screen/Authentication/login_page.dart';
 import 'Screen/task/notification_service.dart';
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  await Firebase.initializeApp(
+
+  );
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
   await SharedPreff.to.initial();
   final AndroidInitializationSettings initializationSettingsAndroid =
   AndroidInitializationSettings('app_icon');
